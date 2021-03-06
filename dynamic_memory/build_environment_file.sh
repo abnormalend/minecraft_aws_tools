fb6751112f9f3cbd22b731e9a45fb7d3cff0756d
abnormalend <brentarogers@gmail.com>
2021-03-06 10:15:45 -0500

    Update build_environment_file.sh

--- dynamic_memory/build_environment_file.sh
+++ dynamic_memory/build_environment_file.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 INSTANCE_TYPE=$(curl http://169.254.169.254/latest/meta-data/instance-type)
 REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
 
 SYSTEM_MEMORY=$(aws ec2 describe-instance-types --region=$REGION --instance-types 't3a.small' --query 'InstanceTypes[0].MemoryInfo.SizeInMiB')
-RESERVED_MEMORY="1024"
+RESERVED_MEMORY=$(aws ec2 describe-instances --region=$REGION --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].Tags[?Key==`reserved_memory`].Value' --output text)
 MAXMEM=$(expr $SYSTEM_MEMORY - $RESERVED_MEMORY)
 MAXMEMUNIT="M"
 MCMAXMEM=$MAXMEM$MAXMEMUNIT
