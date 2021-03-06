1daac2dd503ae654b5af32316047508e31e88dc9
abnormalend <brentarogers@gmail.com>
2021-03-06 10:39:42 -0500

    don't hardcode the instance type, it's a variable now

--- dynamic_memory/build_environment_file.sh
+++ dynamic_memory/build_environment_file.sh
@@ -3,7 +3,7 @@ INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 INSTANCE_TYPE=$(curl http://169.254.169.254/latest/meta-data/instance-type)
 REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
 
-SYSTEM_MEMORY=$(aws ec2 describe-instance-types --region=$REGION --instance-types 't3a.small' --query 'InstanceTypes[0].MemoryInfo.SizeInMiB')
+SYSTEM_MEMORY=$(aws ec2 describe-instance-types --region=$REGION --instance-types '$INSTANCE_TYPE' --query 'InstanceTypes[0].MemoryInfo.SizeInMiB')
 RESERVED_MEMORY=$(aws ec2 describe-instances --region=$REGION --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].Tags[?Key==`reserved_memory`].Value' --output text)
 MAXMEM=$(expr $SYSTEM_MEMORY - $RESERVED_MEMORY)
 MAXMEMUNIT="M"
