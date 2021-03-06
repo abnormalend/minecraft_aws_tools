ecdb4f4f56a817896c0416a5252fe5769a7e3777
abnormalend <brentarogers@gmail.com>
2021-03-06 18:06:49 -0500

    add dynamic memory to installer

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ cp $MINECRAFT_TOOLS_HOME/playercount_metric/crontab /etc/cron.d/minecraftmetrics
 
 # Install paper updater
 cp $MINECRAFT_TOOLS_HOME/paper_updater/crontab /etc/cron.d/paperupdater
+
+# Install dynamic memory
+cp $MINECRAFT_TOOLS_HOME/dynamic_memory/crontab /etc/cron.d/dynamicmemory
